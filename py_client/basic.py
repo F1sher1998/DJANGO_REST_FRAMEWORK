62f6a83cdb5ed6c637771b86d37f606760450fab
F1sher1998 <ivanprotsyk1998@gmail.com>
2025-07-02 12:53:55 +0200

    Comm 1

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,9 @@ import requests
 
 
 endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+endpoint = "https://httpbin.org/"
 
-get_response = requests.get(endpoint) #API
+get_response = requests.get(endpoint, json={"query":"Hello World"}) #API
 print(get_response.text) #raw ext response
+print(get_response.json())
+print(get_response.status_code)
